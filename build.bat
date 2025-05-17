359411e77da6e2ed77f37ec44326600a0ef3499c
doice68 <94632171+mehhrab@users.noreply.github.com>
2025-05-17 06:34:41 -0700

    fixed build.bat again

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 if not exist build mkdir build
 cd build
-if %1==debug odin run ../src -debug -out:Darko.exe
-if %1==release odin run ../src -out:Darko.exe
+if "%1"=="release" odin run ../src -out:Darko.exe
+if "%1"=="debug" odin run ../src -debug -out:Darko.exe
 cd ..
